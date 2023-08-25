b3ff560b6b2c6fbae0c1e433493355540754e872
ipluchino <ianlikesmath@gmail.com>
2023-08-24 22:10:27 -0400

    Add project files.

---
+++ HDTPlugin/SpellSchool.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net.Http.Headers;
+using HearthDb.Enums;
+using Hearthstone_Deck_Tracker;
+using Hearthstone_Deck_Tracker.Hearthstone;
+using CoreAPI = Hearthstone_Deck_Tracker.API.Core;
+
+namespace SpellSchoolCounter
+{
+    internal class SpellSchoolCounter
+    {
+        private SchoolCountWidget _cardListWidget = null;
+        private List<SpellSchool> _schoolsPlayed = new List<SpellSchool>();
+        private ObservableCollection<Card> _playedList = new ObservableCollection<Card>();
+
+        public SpellSchoolCounter(SchoolCountWidget playerList)
+        {
+            _cardListWidget = playerList;
+
+            // Hide in menu, if necessary
+            if (Config.Instance.HideInMenu && CoreAPI.Game.IsInMenu)
+            {
+                _cardListWidget.Hide();
+            }
+
+        }
+
+        // Reset on when a new game starts
+        internal void GameStart()
+        {
+            _schoolsPlayed = new List<SpellSchool>();
+            _playedList = new ObservableCollection<Card>();
+            _cardListWidget.Show();
+            _cardListWidget.Update(_playedList);
+        }
+
+        internal void OnPlayerPlay(Card card)
+        {
+            if (card.SpellSchool != SpellSchool.NONE && !_schoolsPlayed.Contains(card.SpellSchool))
+            {
+                _schoolsPlayed.Add(card.SpellSchool);
+                _playedList.Add(card);
+                _cardListWidget.Update(_playedList);
+            }
+        }
+
+        // Need to handle hiding the element when in the game menu
+        internal void InMenu()
+        {
+            if (Config.Instance.HideInMenu)
+            {
+                _cardListWidget.Hide();
+
+            }
+        }
+    }
+}
---
+++ HDTPlugin/SpellSchoolPlugin.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows.Controls;
+using Hearthstone_Deck_Tracker.API;
+using Hearthstone_Deck_Tracker.Plugins;
+
+namespace SpellSchoolCounter
+{
+    public class SpellSchoolCounterPlugin : IPlugin
+    {
+        private SchoolCountWidget _widget;
+
+        public string Author
+        {
+            get { return "ipluchino"; }
+        }
+
+        public string ButtonText
+        {
+            get { return "Settings"; }
+        }
+
+        public string Description
+        {
+            get { return "Tracks spell schools that have been cast."; }
+        }
+
+        public MenuItem MenuItem
+        {
+            get { return null; }
+        }
+
+        public string Name
+        {
+            get { return "Spell School Tracking"; }
+        }
+
+        public void OnButtonPress()
+        {
+        }
+
+        public void OnLoad()
+        {
+            _widget = new SchoolCountWidget();
+
+            Core.OverlayCanvas.Children.Add(_widget);
+
+            SpellSchoolCounter counter = new SpellSchoolCounter(_widget);
+
+            GameEvents.OnGameStart.Add(counter.GameStart);
+            GameEvents.OnInMenu.Add(counter.InMenu);
+            GameEvents.OnPlayerPlay.Add(counter.OnPlayerPlay);
+        }
+
+        public void OnUnload()
+        {
+            Core.OverlayCanvas.Children.Remove(_widget);
+        }
+
+        public void OnUpdate()
+        {
+        }
+
+        public Version Version
+        {
+            get { return new Version(1, 0, 0); }
+        }
+    }
+}
